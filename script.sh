28f180fc45f9826aa6a240ace8b0ecfcbd39f3f5
João Mourão Sá <joao.mourao.sa@gmail.com>
2022-05-18 06:12:01 +0100

    script more or less finished

--- script.sh
+++ script.sh
@@ -3,89 +3,133 @@
 GREEN=$'\e[0;32m'
 RED=$'\e[0;31m'
 NC=$'\e[0m'
+BOLD=$(tput bold)
+REGULAR=$(tput sgr0)
 
-NODE_CONTAINER='mock-deploy_nodeserver_1'
-NGINX_CONTAINER='mock-deploy_nginx_1'
-
-NODE_REGISTRY='ghcr.io/joaomouraosa/mock_nodeserver:latest'
-NGINX_REGISTRY='ghcr.io/joaomouraosa/mock_nginx:certified'
-
-REPO="joaomouraosa/mock-deploy.git"
+## Git
+REPO_HANDLER='mock-deploy'
+GIT_USERNAME='joaomouraosa'
 TOKEN="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"
+REPO="$GIT_USERNAME/$REPO_HANDLER.git"
+
+## docker-compose
+NODE_SERVICE='nodeserver'
+NGINX_SERVICE='nginx'
+
+## docker registry
+NODE_CONTAINER="${REPO_HANDLER}_${NODE_SERVICE}_1"
+NGINX_CONTAINER="${REPO_HANDLER}_${NGINX_SERVICE}_1"
+
+NODE_REGISTRY="ghcr.io/$GIT_USERNAME/${REPO_HANDLER}_${NODE_SERVICE}:latest"
+NGINX_REGISTRY="ghcr.io/$GIT_USERNAME/${REPO_HANDLER}_${NODE_SERVICE}:latest"
+
+## VPS
+INSTANCE="instance-2 --zone=europe-west1-b"
+
+
+pp() {
+    echo ${BOLD}$1${REGULAR}  $2
+}
+
+
+testURL() {
+    result=`curl -s -o /dev/null -w "%{http_code}" --max-time 3 $1`
+    if [ $result == "200" ]; then echo ${GREEN}${BOLD}'O'${REGULAR}${NC} $URL 
+    else echo ${RED}${BOLD}'X'${REGULAR}${NC} $URL 
+    fi
+}
 
 
 test() {
-    URLs=('http://localhost:5000' 'http://localhost:80' 'http://localhost' 'http://fastfix.shop' 'http://www.fastfix.shop' 'https://fastfix.shop' 'https://www.fastfix.shop')
-    for URL in ${URLs[@]}; do
-        result=`curl -s -o /dev/null -w "%{http_code}" --max-time 3 $URL`
-        if [ $result == "200" ]; then
-            echo $URL ${GREEN} 'Success' ${NC}
-        else
-            echo $URL ${RED} 'Failed' ${NC}
-        fi
-    done
+    pp 'Locally' && URLs=('http://localhost:5000' 'http://localhost:80' 'http://localhost'); for URL in ${URLs[@]}; do testURL $URL; done
+    pp 'VPS - Without SSL' && URLs=('http://fastfix.shop' 'http://www.fastfix.shop'); for URL in ${URLs[@]}; do testURL $URL; done
+    pp 'VPS - With SSL' && URLs=('https://fastfix.shop' 'https://www.fastfix.shop'); for URL in ${URLs[@]}; do testURL $URL; done
 }
-
-# sudo docker container prune  # remove stopped containers
-# sudo docker image ls | tr -s ' ' | cut -d " " -f 3 | sudo xargs docker image rm  # remove all images 
-
-
-# sudo docker-compose down # stop the running containers (1)
-# 
 
 
 build() {
-    echo '== [1/2] Building...' && docker-compose up --build --no-start nodeserver #nginx
-    echo '== [2/2] Pushing...'  && docker push $NODE_REGISTRY #&& docker push $NGINX_REGISTRY
+    pp '== Build [1/3] Build'  && docker-compose up --build --no-start nodeserver #nginx
+    pp '== Build [2/3] Commit' && docker commit $NODE_CONTAINER $NODE_REGISTRY
+    pp '== Build [3/3] Push'   && docker push $NODE_REGISTRY #&& docker push $NGINX_REGISTRY
 }
 
 
 run() {
-    echo '== [1/2] Pulling...' && docker pull $NODE_REGISTRY && docker pull $NGINX_REGISTRY
-    echo '== [1/2] Running...' && docker-compose up --no-build 
+    pp '== Run locally  [1/2] Pull'  && docker pull $NODE_REGISTRY && docker pull $NGINX_REGISTRY
+    pp '== Run locally  [2/2] Run'   && docker-compose up --no-build -d
+}
+
+
+clean() {
+    pp '== Stop [1/2] Stop'   && docker-compose down --rmi all
+    pp '== Stop [2/2] Clean'  && docker system prune -f
+}
+
+
+exit() {
+    clean
+    pp '== Exit [1/1] Close instance' && gcloud compute instances stop $INSTANCE
 }
 
 
 deploy() {
-    echo '== [1/6] Start the instance...'       && gcloud compute instances start instance-2 --zone="europe-west1-b"
-    echo '== [2/6] Kill nginx services...'      && gcloud compute ssh instance-2 --zone="europe-west1-b" --command="sudo systemctl stop nginx && sudo killall nginx"
-    echo '== [3/6] Cloning/Pulling the repo...' && gcloud compute ssh instance-2 --zone="europe-west1-b" --command="git clone https://$TOKEN@$REPO; cd mock-deploy && git pull"
-    echo '== [4/6] Stop running containers...'  && gcloud compute ssh instance-2 --zone="europe-west1-b" --command="cd mock-deploy && sudo docker-compose down; sudo docker system prune -f"
-    echo '== [5/6] Pulling the images...'       && gcloud compute ssh instance-2 --zone="europe-west1-b" --command="sudo docker pull $NODE_REGISTRY && sudo docker pull $NGINX_REGISTRY"
-    echo '== [6/6] Running...'                  && gcloud compute ssh instance-2 --zone="europe-west1-b" --command="cd mock-deploy && sudo docker-compose up --no-build"
+    pp '== Deploy [1/6] Start the instance...'       && gcloud compute instances start $INSTANCE
+    pp '== Deploy [2/6] Kill nginx services...'      && gcloud compute ssh $INSTANCE --command="sudo systemctl stop nginx; sudo killall nginx; sudo systemctl disable nginx"
+    pp '== Deploy [3/6] Cloning/Pulling the repo...' && gcloud compute ssh $INSTANCE --command="git clone https://$TOKEN@$REPO; cd mock-deploy && git pull"
+    pp '== Deploy [4/6] Stop running containers...'  && gcloud compute ssh $INSTANCE --command="cd mock-deploy && sudo docker-compose down; sudo docker system prune -f"
+    pp '== Deploy [5/6] Pulling the images...'       && gcloud compute ssh $INSTANCE --command="sudo docker pull $NODE_REGISTRY && sudo docker pull $NGINX_REGISTRY"
+    pp '== Deploy [6/6] Run'                         && gcloud compute ssh $INSTANCE --command="cd mock-deploy && sudo docker-compose up --no-build -d"
 }
 
 ssl() {
-    echo 'Not automated - steps:'
-    echo '======================'
-    echo '== [1/6] Start the instance...' && echo "$ gcloud compute instances start instance-2 --zone=\"europe-west1-b\""
+    pp 'Not automated - steps:'
+    pp '======================'
+    pp '# Local [1/6] Start instance & access the instance' 
+    echo "gcloud compute instances start $INSTANCE"
+    echo "gcloud compute ssh $INSTANCE"
     echo ''
-    echo '== [2/6] Access the instance via SSH...' && echo "$ gcloud compute ssh instance-2 --zone=\"europe-west1-b\""
+    pp '# Instance [2/6] Access container in instance' 
+    echo "cd mock-deploy"
+    echo "sudo docker-compose exec nginx sh"
+    echo "#sudo docker exec -it $NGINX_CONTAINER sh  # (alternative)"  
     echo ''
-    echo '== [3/6] Access the docker container...' && echo "$ sudo docker exec -it mock-deploy_nginx_1 sh"
+    pp '# Container [4/6] Update the SSL credentials...' 
+    echo "certbot --nginx -d fastfix.shop -d www.fastfix.shop"
     echo ''
-    echo '== [4/6] Update the SSL credentials...' && echo "$ certbot --nginx -d fastfix.shop -d www.fastfix.shop"
+    pp '# Container [5/6] Close the container...' 
+    echo "# exit"
     echo ''
-    echo '== [5/6] Close the container...' && echo "$ exit"
-    echo ''
-    echo '== [6/6] Push the updated image...' && echo "$ sudo docker push ghcr.io/joaomouraosa/mock_nginx:latest"
+    pp '# Instance [5/6] Commit & Push the new image...' 
+    echo "sudo docker commit $NGINX_CONTAINER $NGINX_REGISTRY"
+    echo "sudo docker push $NGINX_REGISTRY"
     echo ''
 }
 
-all() {
-    echo 'all'
+
+help() {
+    pp '=============== Help ==============='
+    echo '-b | --build   Builds locally'
+    echo '-r | --run     Runs locally'
+    echo '-d | --deploy  Deploys to the VPS'
+    echo '-t | --test    Connection tests'
+    echo '-a | --all     i.e., -b -r -d -t'
+    echo '-s | --ssl     SSL setup steps'
+    echo '-e | --exit    Closes the instance'
+    echo '-h | --help    Prompts this'
 }
 
 
-for arg; do 
+for arg; do     
     case "$arg" in
         -b|--build) build;;
         -d|--deploy) deploy;;
         -r|--run) run;;
         -s|--ssl) ssl;;
         -t|--test) test;;
-        -a|--all) echo "all";;
-        *) echo "Usage: (-s|--scan)";;
+        -a|--all|-brdt|--) build && run && deploy && sleep 5s && test;;
+        -c|--clean) clean;;
+        -e|--exit) exit && sleep 5s && test;;
+        -h|--help|*) help;;
     esac
 done
 
